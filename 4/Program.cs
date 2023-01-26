72efd2368de231762069d4322931e5b2a08c2643
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-01-26 22:16:58 +0300

    4

---
+++ 4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number < 100 || number > 999){
+     Console.WriteLine("Введен не правильный номер");
+     return;
+}
+int otvet = number % 10;
+ Console.WriteLine($"3 цифра: {otvet}");
+
