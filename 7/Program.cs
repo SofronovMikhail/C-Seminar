7006696967a8c38d89b2c38781a5d4d9c5d08b00
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-01-30 21:09:25 +0300

    7

---
+++ 7/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+if(numberA % numberB == 0){
+    Console.WriteLine("кратно");
+}
+else{
+   Console.WriteLine($"не кратно, остаток - {numberA % numberB}"); 
+}
