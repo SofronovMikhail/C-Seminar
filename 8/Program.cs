1e6ac03da5aa632ca2586214a5fce29bcb035f9a
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-01-30 21:29:21 +0300

    8

---
+++ 8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+if(numberA % 7 == 0){
+    if(numberA % 23 ==0){
+    Console.WriteLine("кратно"); }
+}
+
+else{
+   Console.WriteLine("не кратно"); 
+}
+
