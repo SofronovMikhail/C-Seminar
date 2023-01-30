079cf19d279623af9beb62830bdaf97db6c9185b
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-01-30 20:22:54 +0300

    5

---
+++ 5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int num = new Random().Next(10, 100);
+Console.WriteLine($"Num = {num}");
+int num1 = num / 10;
+int num2 = num % 10;
+int max =num2;
+if(num1 > max){
+    max = num1;
+}
+Console.WriteLine($"Max = {max}");
