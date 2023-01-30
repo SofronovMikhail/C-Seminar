ab7ab37abcd731db0032921f2ddad472242cd494
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-01-30 21:55:56 +0300

    9

---
+++ 9/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+if(numberA * numberA == numberB){
+    Console.WriteLine("Да");
+    return;
+}
+if(numberB * numberB == numberA){
+    Console.WriteLine("Да");
+}
+else{
+   Console.WriteLine("Нет"); 
+}
