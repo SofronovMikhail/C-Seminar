c36241d36bb52340ffa9fad7a1daab66043c0fcc
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-02-03 21:11:35 +0300

    10

---
+++ 10/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите число X: ");
+int X = int.Parse(Console.ReadLine());
+Console.Write("Введите число Y: ");
+int Y = int.Parse(Console.ReadLine());
+if(X>0&&Y>0){
+   Console.WriteLine("1"); 
+}
+if(X<0&&Y>0){
+   Console.WriteLine("2"); 
+}
+if(X<0&&Y<0){
+   Console.WriteLine("3"); 
+}
+if(X>0&&Y<0){
+   Console.WriteLine("4"); 
+}
