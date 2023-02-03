d691d15cd1d4a6e4c1517b6e98b19be692c18b2a
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-02-03 21:35:33 +0300

    11

---
+++ 11/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+Console.Write("Введите номер четверти: ");
+int N = int.Parse(Console.ReadLine());
+switch(N){
+    case 1:
+    {
+        Console.WriteLine("X>0  Y>0"); 
+        break;
+    }
+   case 2:
+    {
+        Console.WriteLine("X<0  Y>0"); 
+        break;
+    }
+    case 3:
+    {
+        Console.WriteLine("X<0  Y<0"); 
+        break;
+    }
+    case 4:
+    {
+        Console.WriteLine("X>0  Y<0"); 
+        break;
+    }
+    default:
+    {
+        Console.WriteLine("Введено не правильное число"); 
+        break;
+    }
+}
