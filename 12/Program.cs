a6e34b6288da960224901fc74801f553ca00d910
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-02-03 22:17:05 +0300

    12

---
+++ 12/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите координаты по Х1: ");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("Введите координаты по Y1: ");
+int y1 = int.Parse(Console.ReadLine());
+Console.Write("Введите координаты по Х2: ");
+int x2 = int.Parse(Console.ReadLine());
+Console.Write("Введите координаты по Y2: ");
+int y2 = int.Parse(Console.ReadLine());
+double s = Math.Sqrt(Math.Pow(x1 - x2, 2)) + (Math.Pow(x1 - x2, 2));
+Console.WriteLine($"s = {s:f3}");
+//Так же можно все int поменять на double
